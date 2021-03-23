5c96edd2ccbad0ebb6a31cf69cee75392010139f
Franklin Waller <fr.l.waller@gmail.com>
2021-03-23 13:14:55 +0100

    chore: Remove configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
-// Connect with the NEAR api
-// Connect with the RPC (through the near-api-js)
-// Use that to talk to the smart contract
-// Check if we can listen to logs and act on those
-
 import yargs from "yargs";
 
 import { start } from "./commands/start";
