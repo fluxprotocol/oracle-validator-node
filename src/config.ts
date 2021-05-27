f80621968a119094ca18ff4668fb53573f2c8326
Franklin Waller <fr.l.waller@gmail.com>
2021-05-27 15:22:59 +0200

    feat: Implement storage depositing for near

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,10 @@
 import Big from 'big.js';
 
-Big.PE = 100000;
+Big.PE = 100_000;
 
-export const JOB_SEARCH_INTERVAL = 5000;
-export const BALANCE_REFRESH_INTERVAL = 10000;
-export const CLAIM_CHECK_INTERVAL = 10000;
+export const JOB_SEARCH_INTERVAL = 5_000;
+export const BALANCE_REFRESH_INTERVAL = 10_000;
+export const CLAIM_CHECK_INTERVAL = 10_000;
+export const STORAGE_DEPOSIT_CHECK_INTERVAL = 100_000;
 export const JOB_WALKER_INTERVAL = JOB_SEARCH_INTERVAL;
 export const TOKEN_DENOM = 18;
--- src/providers/Near/NearService.ts
+++ src/providers/Near/NearService.ts
@@ -1,4 +1,4 @@
-import { Account, connect, keyStores, Near, providers, utils } from "near-api-js";
+import { Account, connect, keyStores, Near, providers, utils, WalletConnection, transactions } from "near-api-js";
 import path from 'path';
 
 import { createNearNetworkConfig, NetworkType } from "../../models/NearNetworkConfig";
@@ -48,4 +48,43 @@ export function extractLogs(executionOutcome: providers.FinalExecutionOutcome) {
     });
 
     return logs;
+}
+
+export interface TransactionViewOptions {
+    methodName: string;
+    args?: object;
+}
+
+export interface TransactionCallOptions extends TransactionViewOptions {
+    gas: string;
+    amount: string;
+}
+
+export interface TransactionOption {
+    receiverId: string;
+    transactionOptions: TransactionCallOptions[];
+}
+
+export async function batchSendTransactions(walletConnection: WalletConnection, txs: TransactionOption[], callbackUrl?: string) {
+    const accountId = walletConnection.getAccountId();
+    const localKey = await walletConnection._near.connection.signer.getPublicKey(accountId, walletConnection._near.connection.networkId);
+    const block = await walletConnection._near.connection.provider.block({ finality: 'final' });
+    const blockHash = utils.serialize.base_decode(block.header.hash);
+
+    const resultTxs = await Promise.all(txs.map(async ({ receiverId, transactionOptions }, index) => {
+        // @ts-ignore
+        const actions = transactionOptions.map(tx => transactions.functionCall(tx.methodName, tx.args ?? {}, tx.gas, tx.amount));
+        const accessKey = await walletConnection.account().accessKeyForTransaction(receiverId, actions, localKey);
+
+        if (!accessKey) {
+            throw new Error(`Cannot find matching key for transaction sent to ${receiverId}`);
+        }
+
+        const publicKey = utils.PublicKey.from(accessKey.public_key);
+        const nonce = accessKey.access_key.nonce + index + 1;
+
+        return transactions.createTransaction(accountId, publicKey, receiverId, nonce, actions, blockHash);
+    }));
+
+    return walletConnection.requestSignTransactions(resultTxs, callbackUrl);
 }
