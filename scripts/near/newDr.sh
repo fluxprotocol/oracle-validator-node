ed75c3baa8f539771e9a62e9a9651995380b72f5
Franklin Waller <fr.l.waller@gmail.com>
2021-10-13 14:53:05 +0200

    chore(scripts): Make account_id variable

--- scripts/near/newDr.sh
+++ scripts/near/newDr.sh
@@ -1,5 +1,6 @@
 TIME=`date +%s`
 MULTIPLIER=1000
+ACCOUNT_ID=franklinwaller2.testnet
 
 END_TIME=`expr $TIME - 1000`
 END_TIME=`expr $END_TIME \* $MULTIPLIER`
@@ -10,4 +11,4 @@ BEGIN_TIME=`expr $END_TIME - $MINUS_BEGIN_TIME`
 SOURCES="[{ \"source_path\": \"0.4\", \"end_point\": \"https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&startTime=$BEGIN_TIME&endTime=$END_TIME&limit=1\" }]"
 JSON="{\"sources\": $SOURCES,\"tags\":[\"1\"],\"description\":\"What is the price of BTC?\",\"challenge_period\":\"120000000000\",\"settlement_time\":\"1\",\"data_type\":{\"Number\":\"10000000000\"},\"creator\":\"requestor-contract.flux-dev\"}"
 
-env NEAR_ENV=testnet near call requestor-contract.flux-dev create_data_request "{\"amount\": \"1000000000000000000000000\", \"payload\": $JSON}" --accountId franklinwaller2.testnet --amount 0.000000000000000000000001 --gas=300000000000000
+env NEAR_ENV=testnet near call requestor-contract.flux-dev create_data_request "{\"amount\": \"1000000000000000000000000\", \"payload\": $JSON}" --accountId $ACCOUNT_ID --amount 0.000000000000000000000001 --gas=300000000000000
